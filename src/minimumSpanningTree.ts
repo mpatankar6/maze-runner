487741663797d3c5ea5d88d7899b2a72200862dc
Mihir Patankar <mpatankar06@gmail.com>
2025-04-15 02:12:39 -0400

    Improve clarity of mst implementation

--- src/minimumSpanningTree.ts
+++ src/minimumSpanningTree.ts
@@ -3,16 +3,14 @@ import { UnionFind } from "./unionFind";
 
 /**
  * Computes the minimum spanning tree of a graph using Kruskal's algorithm.
- *
  * @param edges - The array of edges that define the graph.
  * @param numCells - The total number of cells in the graph.
- * @returns An array of edges representing the MST.
- *
+ * @returns An array of edges representing the minimum spanning tree.
  */
 export function minimumSpanningTree(edges: Edge[], numCells: number) {
   const mstEdges: Edge[] = [];
   // Descending order so Array.pop() returns the smallest edge
-  const worklist = [...edges].sort((a, b) => b.weight - a.weight);
+  const worklist = edges.sort((a, b) => b.weight - a.weight);
   const unionFind = new UnionFind();
 
   while (mstEdges.length < numCells - 1 && worklist.length > 0) {
