ede59efe66172e4ab5b62b8aed13266286cda408
Mihir Patankar <mpatankar06@gmail.com>
2025-08-21 15:53:26 -0400

    Add UI

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,42 @@ import "./styles/style.css";
 const canvasElement = document.getElementById(
   "maze-window"
 ) as HTMLCanvasElement | null;
-if (!canvasElement) throw new Error("No #maze-window canvas element found.");
+if (!canvasElement) throw new Error("Canvas element not found.");
 
-const rows = 30;
-const cols = 30;
+const heightInput = document.getElementById(
+  "maze-height-field"
+) as HTMLInputElement;
+if (!heightInput) throw new Error("Height field not found.");
 
-let grid = new Grid(rows, cols, canvasElement);
+const widthInput = document.getElementById(
+  "maze-width-field"
+) as HTMLInputElement;
+if (!widthInput) throw new Error("Width field not found.");
+
+[heightInput, widthInput].forEach((input) => {
+  input.addEventListener("keydown", (event) => {
+    const key = (event as KeyboardEvent).key;
+    if (!/^[0-9]$/.test(key) && key !== "Backspace" && key !== "Tab") {
+      event.preventDefault();
+    }
+  });
+
+  input.addEventListener("blur", (event: Event) => {
+    const minDimensionSizeCells = 2;
+    const maxDimensionSizeCells = 200;
+    const target = event.currentTarget as HTMLInputElement;
+    // Clamps value between min and max sizes.
+    target.value = Math.min(
+      Math.max(Number(target.value), minDimensionSizeCells),
+      maxDimensionSizeCells
+    ).toString();
+  });
+});
+
+const mazeHeight = () => Number(heightInput.value);
+const mazeWidth = () => Number(widthInput.value);
+
+let grid = new Grid(mazeHeight(), mazeWidth(), canvasElement);
 grid.drawCellsToCanvas();
 
 let mazeCompleted = false;
@@ -26,53 +56,78 @@ function finalizeMaze() {
 document.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "ArrowUp":
-      if (!mazeCompleted) grid.movePlayerIfPossible(0, -1);
+      if (!mazeCompleted && gridAnimator.isInactive())
+        grid.movePlayerIfPossible(0, -1);
       break;
     case "ArrowLeft":
-      if (!mazeCompleted) grid.movePlayerIfPossible(-1, 0);
+      if (!mazeCompleted && gridAnimator.isInactive())
+        grid.movePlayerIfPossible(-1, 0);
       break;
     case "ArrowDown":
-      if (!mazeCompleted) grid.movePlayerIfPossible(0, 1);
+      if (!mazeCompleted && gridAnimator.isInactive())
+        grid.movePlayerIfPossible(0, 1);
       break;
     case "ArrowRight":
-      if (!mazeCompleted) grid.movePlayerIfPossible(1, 0);
-      break;
-    case "r":
-      mazeCompleted = false;
-      gridAnimator.cancelCurrentAnimation();
-      grid = new Grid(rows, cols, canvasElement as HTMLCanvasElement);
-      grid.drawCellsToCanvas();
-      break;
-    case "b":
-      grid.drawCellsToCanvas();
-      gridAnimator.animateSearch(
-        grid.searchGrid(breadthFirstSearch),
-        finalizeMaze
-      );
-      break;
-    case "d":
-      grid.drawCellsToCanvas();
-      gridAnimator.animateSearch(
-        grid.searchGrid(depthFirstSearch),
-        finalizeMaze
-      );
-      break;
-    case "z":
-      if (gridAnimator.isClear())
-        grid.colorCellsByDistanceRelativeToStartingCell();
-      break;
-    case "x":
-      if (gridAnimator.isClear())
-        grid.colorCellsByDistanceRelativeToEndingCell();
-      break;
-    case "c":
-      if (gridAnimator.isClear()) grid.drawCellsToCanvas();
+      if (!mazeCompleted && gridAnimator.isInactive())
+        grid.movePlayerIfPossible(1, 0);
       break;
     default:
-      console.log("Unrecognized keystroke: ", event.key);
       break;
   }
   if (grid.playerWon()) {
     finalizeMaze();
   }
+});
+
+const resetButton = document.getElementById("reset-button");
+if (!resetButton) throw new Error("Reset button not found.");
+resetButton.addEventListener("click", () => {
+  mazeCompleted = false;
+  gridAnimator.cancelCurrentAnimation();
+  grid = new Grid(
+    mazeHeight(),
+    mazeWidth(),
+    canvasElement as HTMLCanvasElement
+  );
+  grid.drawCellsToCanvas();
+});
+
+const bfsButton = document.getElementById("bfs-button");
+if (!bfsButton) throw new Error("BFS button not found.");
+bfsButton.addEventListener("click", () => {
+  grid.drawCellsToCanvas();
+  gridAnimator.animateSearch(grid.searchGrid(breadthFirstSearch), finalizeMaze);
+});
+
+const dfsButton = document.getElementById("dfs-button");
+if (!dfsButton) throw new Error("DFS button not found.");
+dfsButton.addEventListener("click", () => {
+  grid.drawCellsToCanvas();
+  gridAnimator.animateSearch(grid.searchGrid(depthFirstSearch), finalizeMaze);
+});
+
+const heatmapRelativeToStartButton = document.getElementById(
+  "heatmap-relative-to-start-button"
+);
+if (!heatmapRelativeToStartButton)
+  throw new Error("Heatmap relative to start button not found.");
+heatmapRelativeToStartButton.addEventListener("click", () => {
+  if (gridAnimator.isInactive())
+    grid.colorCellsByDistanceRelativeToStartingCell();
+});
+
+const heatmapRelativeToEndButton = document.getElementById(
+  "heatmap-relative-to-end-button"
+);
+if (!heatmapRelativeToEndButton)
+  throw new Error("Heatmap relative to end button not found.");
+heatmapRelativeToEndButton.addEventListener("click", () => {
+  if (gridAnimator.isInactive())
+    grid.colorCellsByDistanceRelativeToEndingCell();
+});
+
+const clearHeatmapButton = document.getElementById("clear-heatmap-button");
+if (!clearHeatmapButton) throw new Error("Clear heatmap button not found.");
+clearHeatmapButton.addEventListener("click", () => {
+  if (gridAnimator.isInactive()) grid.drawCellsToCanvas();
 });
